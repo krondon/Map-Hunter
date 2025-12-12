486888b14a3f8e7824a7e4a02eb3267d4ba75660
Andres <parraandres723@gmail.com>
2025-12-12 15:59:06 -0400

    refactor: update Android build configuration and clean up metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 
 android {
     namespace = "com.example.treasure_hunt_rpg"
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
+
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -18,19 +20,15 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.treasure_hunt_rpg"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
