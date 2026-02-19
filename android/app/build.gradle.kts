ad086cfa9569cbe95ddc6503b6c0bd5c247e7ddf
unknown <danielecd18@gmail.com>
2026-02-19 11:07:59 -0400

    modificando variables para subir el dominio en play store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.treasure_hunt_rpg"
+    namespace = "com.map.hunter"
     compileSdk = flutter.compileSdkVersion
 
     ndkVersion = "27.0.12077973"
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.treasure_hunt_rpg"
+        applicationId = "com.map.hunter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
