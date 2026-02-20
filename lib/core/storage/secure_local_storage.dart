2c7f3436f8ac0eb53294499e0f04c5b417b6d99e
Andres <parraandres723@gmail.com>
2026-02-19 23:18:33 -0400

    DEMO secure local storage

--- lib/core/storage/secure_local_storage.dart
+++ lib/core/storage/secure_local_storage.dart
@@ -2,29 +2,36 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 /// Implementación segura del almacenamiento local para Supabase Auth.
-/// Utiliza Keychain (iOS) y Keystore (Android) para encriptar el tokens.
+/// Utiliza Keychain (iOS), Keystore (Android) y libsecret (Linux) para encriptar el tokens.
 class SecureLocalStorage extends LocalStorage {
-  SecureLocalStorage()
-      : super(
-          initialize: () async {},
-          hasAccessToken: () {
-            const storage = FlutterSecureStorage();
-            return storage.containsKey(key: supabasePersistSessionKey);
-          },
-          accessToken: () {
-            const storage = FlutterSecureStorage();
-            return storage.read(key: supabasePersistSessionKey);
-          },
-          removePersistedSession: () {
-            const storage = FlutterSecureStorage();
-            return storage.delete(key: supabasePersistSessionKey);
-          },
-          persistSession: (String persistSessionString) {
-            const storage = FlutterSecureStorage();
-            return storage.write(
-                key: supabasePersistSessionKey, value: persistSessionString);
-          },
-        );
+  SecureLocalStorage();
 
   static const supabasePersistSessionKey = 'supabase_persist_session';
+  final _storage = const FlutterSecureStorage();
+
+  @override
+  Future<void> initialize() async {
+    // No initialization needed for flutter_secure_storage
+  }
+
+  @override
+  Future<bool> hasAccessToken() async {
+    return _storage.containsKey(key: supabasePersistSessionKey);
+  }
+
+  @override
+  Future<String?> accessToken() async {
+    return _storage.read(key: supabasePersistSessionKey);
+  }
+
+  @override
+  Future<void> removePersistedSession() async {
+    return _storage.delete(key: supabasePersistSessionKey);
+  }
+
+  @override
+  Future<void> persistSession(String persistSessionString) async {
+    return _storage.write(
+        key: supabasePersistSessionKey, value: persistSessionString);
+  }
 }
