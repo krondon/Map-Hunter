c78d53a759a03a5b6b9f0afb9b34694a0aa4664c
Andres <parraandres723@gmail.com>
2026-02-20 12:16:08 -0400

    FIX webview demo

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# 1. Descargar SDK de Flutter (más rápido que clonar)
+curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.5-stable.tar.xz | tar xJ -C /tmp
 
-# 1. Descargar la versión estable de Flutter
-git clone https://github.com/flutter/flutter.git -b stable
+# 2. Agregar Flutter al PATH
+export PATH="/tmp/flutter/bin:$PATH"
 
-# 2. Agregar Flutter a las variables de entorno de Vercel
-export PATH="$PATH:`pwd`/flutter/bin"
-
-# 3. Compilar el proyecto para web
+# 3. Compilar para web
 flutter build web --release
